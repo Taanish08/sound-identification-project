0a2a1102e85e0832e1cb43eebc29f5b5758a2033
Taanish08 <123229789+Taanish08@users.noreply.github.com>
2024-07-12 16:22:21 +0530

    main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function begin() {
+function start() {
     
     navigator.mediaDevices.getUserMedia({audio:true});
 classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/8gi5yMXpw/model.json',modelReady);
